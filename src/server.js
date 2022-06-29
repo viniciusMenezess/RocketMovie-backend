379c320519847ae7bbc100b3e4cd6c6de2a7498e
Vinicius Menezes <vini.ads13@outlook.com>
2022-06-29 15:17:28 -0300

    :tada: add dotenv and pm2

--- src/server.js
+++ src/server.js
@@ -36,5 +36,5 @@ app.use((error,request,response,next) => {
   })
 })
 
-const PORT = process.env.SERVER_PORT || 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`This server is running at http://localhost:${PORT}`))
